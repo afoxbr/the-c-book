7ca2b631ccf3b50afcb5cfa60a1f5a05400c066c
antony <antonyrbramwell@gmail.com>
2023-05-08 21:53:44 +0100

    begin 1-14 main function

--- ex1-14.c
+++ ex1-14.c
@@ -4,5 +4,24 @@
 
 int main()
 {
+        int i, c;
+        int nchar[36];
 
+        for (i = 0; i < 36; ++i)
+            nchar[i] = 0;
+
+        while ((c = getchar()) != EOF){
+            if (c == 'a')
+                ++nchar[0];
+            else if (c == 'b')
+                ++nchar[1];
+            else if (c == 'c')
+                ++nchar[2];
+            else if (c == 'd')
+                ++nchar[3];
+            else if (c == 'e')
+                ++nchar[4];
+        }
+
+        return 0;
 }
